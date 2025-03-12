4d67af536009b556f02db5bea7b1bda035d2ce5b
Nadun Malinda <bknandun@gmail.com>
2025-03-12 18:06:06 +0100

    update: course type

--- src/types/courses.ts
+++ src/types/courses.ts
@@ -1,14 +1,15 @@
 import { z } from "zod";
 
 export const CourseSchema = z.object({
-  CourseId: z.number(),
+  CourseId: z.string(),
   CourseName: z.string(),
   InstituteName: z.string(),
   Category: z.string(),
   DeliveryMethod: z.string(),
   Location: z.string(),
   Language: z.string().optional(),
-  StartDate: z.date(),
+  StartDate: z.string(),
+  IsFavorite: z.boolean().optional(),
 });
 
 export type Course = z.infer<typeof CourseSchema>;
