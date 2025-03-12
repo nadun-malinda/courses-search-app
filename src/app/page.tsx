ef085d71c1077a3578a54bcfc4f2d804c9c00a49
Nadun Malinda <bknandun@gmail.com>
2025-03-12 18:09:35 +0100

    update: main homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { Search } from "@/components/search/search";
+import { type QueryParams } from "@/types/filters";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<QueryParams>;
+}) {
+  const urlSearchParams = await searchParams;
+
   return (
     <div className="container mx-auto transition-all animate-fade-in">
-      <h1>Homepage</h1>
+      <Search urlSearchParams={urlSearchParams} />
     </div>
   );
 }
