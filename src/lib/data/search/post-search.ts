caa96d9d113b6d76fa063576a4ad14b7b797913b
Nadun Malinda <bknandun@gmail.com>
2025-03-14 05:39:23 +0100

    add: post search server function

---
+++ src/lib/data/search/post-search.ts
@@ -0,0 +1,14 @@
+import { createClient } from "@/utils/superbase/server";
+
+export async function postSearch(queryString: string) {
+  const supabase = await createClient();
+  const { error } = await supabase
+    .from("saved_searches")
+    .insert([{ SearchQuery: queryString }]);
+
+  if (error) {
+    throw new Error(error.message); // If there's an error during insert
+  }
+
+  return true;
+}
