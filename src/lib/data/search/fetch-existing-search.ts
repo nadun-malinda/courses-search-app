0d73511b06b857cb1c925cf46bb9ce139c440440
Nadun Malinda <bknandun@gmail.com>
2025-03-14 05:38:44 +0100

    add: fetch exisiting search server function

---
+++ src/lib/data/search/fetch-existing-search.ts
@@ -0,0 +1,16 @@
+import { createClient } from "@/utils/superbase/server";
+
+export async function fetchExistingSearch(queryString: string) {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("saved_searches")
+    .select("Id")
+    .eq("SearchQuery", queryString)
+    .single();
+
+  if (error && error.code !== "PGRST116") {
+    throw new Error(error.message); // Unexpected Supabase error
+  }
+
+  return data;
+}
