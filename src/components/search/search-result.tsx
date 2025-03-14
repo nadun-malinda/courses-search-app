7a1c0f967aa335b1d4d11f9ff10ed5b03568459f
Nadun Malinda <bknandun@gmail.com>
2025-03-14 05:46:31 +0100

    update: search result component to fetch courses data

--- src/components/search/search-result.tsx
+++ src/components/search/search-result.tsx
@@ -1,39 +1,15 @@
-import { z } from "zod";
-import { createClient } from "@/utils/superbase/server";
 import { CourseList } from "../courses/course-list";
-import { CourseSchema } from "@/types/courses";
 import { QueryParams } from "@/types/filters";
+import { fetchCourses } from "@/lib/data/course/fetch-courses";
 
 export async function SearchResult({
   urlSearchParams,
 }: {
   urlSearchParams: QueryParams;
 }) {
-  const { q, category, location } = urlSearchParams;
+  const courses = await fetchCourses(urlSearchParams);
 
-  const supabase = await createClient();
-  let query = supabase.from("courses").select("*");
-
-  if (q) {
-    query = query.ilike("CourseName", `%${q}%`);
-  }
-  if (category) {
-    query = query.in("Category", category.split(","));
-  }
-  if (location) {
-    query = query.in("Location", location.split(","));
-  }
-
-  query.limit(10);
-
-  const { data } = await query;
-  const parsed = z.array(CourseSchema).safeParse(data);
-
-  if (!parsed.success) {
-    return <>Invalid courses data!</>;
-  }
-
-  const courses = parsed.data;
+  console.log(">>> courses: ", courses);
 
   return (
     <div className="space-y-4">
