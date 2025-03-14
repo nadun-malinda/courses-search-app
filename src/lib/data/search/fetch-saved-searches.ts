539fd341d5510fdd24f969d343da8168a43a76ae
Nadun Malinda <bknandun@gmail.com>
2025-03-14 05:39:01 +0100

    add: fetch saved searches server function

---
+++ src/lib/data/search/fetch-saved-searches.ts
@@ -0,0 +1,27 @@
+import { SearchesSchema } from "@/types/searches";
+import { createClient } from "@/utils/superbase/server";
+import { cache } from "react";
+import { z } from "zod";
+
+/**
+ * Fetches saved searches from the Supabase database.
+ * Uses caching to optimize repeated queries.
+ *
+ * @returns {Promise<Array<SearchesSchema>>} A promise resolving to an array of validated saved searches.
+ * @throws {Error} If the fetched data is invalid or if Supabase query fails.
+ */
+export const fetchSavedSearches = cache(async () => {
+  const supabase = await createClient();
+  const { data, error } = await supabase.from("saved_searches").select();
+
+  if (error) {
+    throw new Error(`Failed to fetch saved searches: ${error.message}`);
+  }
+
+  const parsed = z.array(SearchesSchema).safeParse(data);
+  if (!parsed.success) {
+    throw new Error("Invalid saved searches data");
+  }
+
+  return parsed.data;
+});
