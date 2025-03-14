f7909c24887cfa0bfc54275e3c0a821b185c050e
Nadun Malinda <bknandun@gmail.com>
2025-03-14 05:46:04 +0100

    update: search component to use search result component with suspense

--- src/components/search/search.tsx
+++ src/components/search/search.tsx
@@ -1,5 +1,3 @@
-import { Bookmark } from "lucide-react";
-import { Card, CardContent } from "../ui/card";
 import { SearchInput } from "./search-input";
 import { QueryParams } from "@/types/filters";
 import { SearchResult } from "./search-result";
@@ -15,30 +13,12 @@ export function Search({ urlSearchParams }: { urlSearchParams: QueryParams }) {
         </p>
       </div>
 
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
-        <div className="lg:col-span-2 space-y-6">
-          <SearchInput />
+      <div className="lg:col-span-2 space-y-6">
+        <SearchInput />
 
-          <Suspense fallback={<>Loading ...</>}>
-            <SearchResult urlSearchParams={urlSearchParams} />
-          </Suspense>
-        </div>
-
-        <div className="space-y-4">
-          <h2 className="text-xl font-semibold flex items-center">
-            <Bookmark className="h-5 w-5 mr-2" />
-            Saved Searches
-          </h2>
-
-          <Card className="border-dashed bg-muted/30">
-            <CardContent className="pt-6 text-center">
-              <p className="text-muted-foreground">No saved searches yet.</p>
-              <p className="text-sm text-muted-foreground mt-1">
-                Save your search parameters for quick access later.
-              </p>
-            </CardContent>
-          </Card>
-        </div>
+        <Suspense fallback={<>Loading ...</>}>
+          <SearchResult urlSearchParams={urlSearchParams} />
+        </Suspense>
       </div>
     </div>
   );
