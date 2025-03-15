476e857579570f9ba7b158371de7d4c52ffc80fd
Nadun Malinda <bknandun@gmail.com>
2025-03-15 11:41:52 +0100

    update: remove cookie usage from the supabase server client

--- src/utils/superbase/server.ts
+++ src/utils/superbase/server.ts
@@ -1,31 +1,25 @@
 import { createServerClient } from "@supabase/ssr";
 import { type SupabaseClient } from "@supabase/supabase-js";
-import { cookies } from "next/headers";
 
 export type SupabaseClientType = SupabaseClient;
 
-export async function createClient() {
-  const cookieStore = await cookies();
+export const createFetch =
+  (options: Pick<RequestInit, "next" | "cache">) =>
+  (url: RequestInfo | URL, init?: RequestInit) => {
+    return fetch(url, {
+      ...init,
+      ...options,
+    });
+  };
 
+export async function createClient() {
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options)
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
-        },
+        getAll: () => [],
+        setAll: () => {},
       },
     }
   );
