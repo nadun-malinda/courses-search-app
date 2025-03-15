a1845220bfa3519ad55aa8c5510c2ac0c184c538
Nadun Malinda <bknandun@gmail.com>
2025-03-15 11:45:43 +0100

    update: use unstable_cache from nextjs instead from react cache

--- src/lib/data/search/db-fetch-saved-searches.ts
+++ src/lib/data/search/db-fetch-saved-searches.ts
@@ -1,6 +1,6 @@
 import { SearchesSchema } from "@/types/searches";
 import { createClient } from "@/utils/superbase/server";
-import { cache } from "react";
+import { unstable_cache } from "next/cache";
 import { z } from "zod";
 
 /**
@@ -10,18 +10,22 @@ import { z } from "zod";
  * @returns {Promise<Array<SearchesSchema>>} A promise resolving to an array of validated saved searches.
  * @throws {Error} If the fetched data is invalid or if Supabase query fails.
  */
-export const dbFetchSavedSearches = cache(async () => {
-  const supabase = await createClient();
-  const { data, error } = await supabase.from("saved_searches").select();
+export const dbFetchSavedSearches = unstable_cache(
+  async () => {
+    const supabase = await createClient();
+    const { data, error } = await supabase.from("saved_searches").select();
 
-  if (error) {
-    throw new Error(`Failed to fetch saved searches: ${error.message}`);
-  }
+    if (error) {
+      throw new Error(`Failed to fetch saved searches: ${error.message}`);
+    }
 
-  const parsed = z.array(SearchesSchema).safeParse(data);
-  if (!parsed.success) {
-    throw new Error("Invalid saved searches data");
-  }
+    const parsed = z.array(SearchesSchema).safeParse(data);
+    if (!parsed.success) {
+      throw new Error("Invalid saved searches data");
+    }
 
-  return parsed.data;
-});
+    return parsed.data;
+  },
+  ["saved_searches"],
+  { tags: ["saved_searches"] }
+);
