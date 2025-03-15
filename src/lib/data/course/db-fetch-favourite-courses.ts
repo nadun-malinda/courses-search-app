f4388dc3515c2ea346b141bfeb20bc7990bdae01
Nadun Malinda <bknandun@gmail.com>
2025-03-15 11:46:25 +0100

    update: use unstable_cache from nextjs instead from react cache

--- src/lib/data/course/db-fetch-favourite-courses.ts
+++ src/lib/data/course/db-fetch-favourite-courses.ts
@@ -1,29 +1,33 @@
 import { FavouriteCourseSchema } from "@/types/courses";
 import { createClient } from "@/utils/superbase/server";
-import { cache } from "react";
+import { unstable_cache } from "next/cache";
 import { z } from "zod";
 
-export const dbFetchFavouriteCourses = cache(async () => {
-  const supabase = await createClient();
-  const { data, error } = await supabase.from("favourite_courses").select(
-    `*, 
+export const dbFetchFavouriteCourses = unstable_cache(
+  async () => {
+    const supabase = await createClient();
+    const { data, error } = await supabase.from("favourite_courses").select(
+      `*, 
       CoursesTable: courses (CourseName, InstituteName, Category, DeliveryMethod, Location, Language, StartDate, Favorites: favourite_courses (Id))`
-  );
+    );
 
-  if (error) {
-    throw new Error(`Failed to fetch favourite courses: ${error.message}`);
-  }
+    if (error) {
+      throw new Error(`Failed to fetch favourite courses: ${error.message}`);
+    }
 
-  const parsed = z.array(FavouriteCourseSchema).safeParse(data);
-  if (!parsed.success) {
-    throw new Error("Invalid favourite courses data");
-  }
+    const parsed = z.array(FavouriteCourseSchema).safeParse(data);
+    if (!parsed.success) {
+      throw new Error("Invalid favourite courses data");
+    }
 
-  return parsed.data.map((favCourses) => ({
-    ...favCourses,
-    ...favCourses.CoursesTable,
-    IsFavorite: favCourses.CoursesTable
-      ? favCourses.CoursesTable.Favorites.length > 0
-      : false,
-  }));
-});
+    return parsed.data.map((favCourses) => ({
+      ...favCourses,
+      ...favCourses.CoursesTable,
+      IsFavorite: favCourses.CoursesTable
+        ? favCourses.CoursesTable.Favorites.length > 0
+        : false,
+    }));
+  },
+  ["favourite_courses"],
+  { tags: ["favourite_courses"] }
+);
