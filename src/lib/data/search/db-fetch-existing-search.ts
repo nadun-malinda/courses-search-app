dce43fb9e0fb4d1a3aa962aa165bde5e03d78f06
Nadun Malinda <bknandun@gmail.com>
2025-03-15 11:45:57 +0100

    update: use unstable_cache from nextjs instead from react cache

--- src/lib/data/search/db-fetch-existing-search.ts
+++ src/lib/data/search/db-fetch-existing-search.ts
@@ -1,16 +1,21 @@
 import { createClient } from "@/utils/superbase/server";
+import { unstable_cache } from "next/cache";
 
-export async function dbFetchExistingSearch(queryString: string) {
-  const supabase = await createClient();
-  const { data, error } = await supabase
-    .from("saved_searches")
-    .select("Id")
-    .eq("SearchQuery", queryString)
-    .single();
+export const dbFetchExistingSearch = unstable_cache(
+  async (queryString: string) => {
+    const supabase = await createClient();
+    const { data, error } = await supabase
+      .from("saved_searches")
+      .select("Id")
+      .eq("SearchQuery", queryString)
+      .single();
 
-  if (error && error.code !== "PGRST116") {
-    throw new Error(error.message); // Unexpected Supabase error
-  }
+    if (error && error.code !== "PGRST116") {
+      throw new Error(error.message); // Unexpected Supabase error
+    }
 
-  return data;
-}
+    return data;
+  },
+  ["saved_searches"],
+  { tags: ["saved_searches"] }
+);
