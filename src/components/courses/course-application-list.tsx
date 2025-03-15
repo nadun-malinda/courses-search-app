b11e95b2757512ed4a94ccdbc36667a7fdcefbc1
Nadun Malinda <bknandun@gmail.com>
2025-03-15 17:56:06 +0100

    add: list course applications

---
+++ src/components/courses/course-application-list.tsx
@@ -0,0 +1,75 @@
+import { CourseApplication } from "@/types/courses";
+import { Card, CardContent } from "../ui/card";
+import { Badge } from "../ui/badge";
+import { Calendar, GraduationCap } from "lucide-react";
+import { formatDistance } from "date-fns";
+
+export function CourseApplicationList({
+  applications,
+}: {
+  applications: CourseApplication[];
+}) {
+  return (
+    <div className="grid lg:grid-cols-4 md:grid-cols-3 gap-4">
+      {applications.map((application) => (
+        <Card
+          key={application.Id}
+          className="overflow-hidden transition-all-300 hover:shadow-md group h-full border border-border/40 bg-white p-0"
+        >
+          <CardContent className="p-6 flex flex-col h-full">
+            <div className="flex justify-between items-start mb-2">
+              <Badge
+                variant="outline"
+                className="font-normal text-xs bg-secondary mb-2 transition-all-200"
+              >
+                {application.CoursesTable?.Category}
+              </Badge>
+            </div>
+
+            <h3 className="font-medium text-lg mb-1 text-balance leading-tight">
+              {application.CoursesTable?.CourseName}
+            </h3>
+
+            <div className="flex items-center text-muted-foreground text-sm">
+              <GraduationCap className="h-4 w-4 mr-2 flex-shrink-0" />
+              <span>{application.CoursesTable?.InstituteName}</span>
+            </div>
+
+            <div className="space-y-2 mt-auto">
+              <div className="flex items-center text-muted-foreground text-sm">
+                <Calendar className="h-4 w-4 mr-2 flex-shrink-0" />
+                <span>
+                  Applied{" "}
+                  {formatDistance(application.CreatedAt, new Date(), {
+                    addSuffix: true,
+                  })}
+                </span>
+              </div>
+            </div>
+
+            <div className="text-sm mt-4">
+              <div>
+                <span>First name: </span>
+                <span className="text-muted-foreground">
+                  {application.FirstName}
+                </span>
+              </div>
+              <div>
+                <span>Last name: </span>
+                <span className="text-muted-foreground">
+                  {application.LastName}
+                </span>
+              </div>
+              <div>
+                <span>Email: </span>
+                <span className="text-muted-foreground">
+                  {application.Email}
+                </span>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      ))}
+    </div>
+  );
+}
