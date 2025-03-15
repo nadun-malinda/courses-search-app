d00105d202244fde392a7b8881fc09d31f6545eb
Nadun Malinda <bknandun@gmail.com>
2025-03-15 17:54:04 +0100

    update: navigation

--- src/components/navbar/navigation.tsx
+++ src/components/navbar/navigation.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import {
   NavigationMenu,
@@ -5,38 +7,47 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "../ui/navigation-menu";
-import { Heart, Home, Save } from "lucide-react";
+import { FileText, Heart, Home, Save } from "lucide-react";
+import { usePathname } from "next/navigation";
+import { Button } from "../ui/button";
 
+/**
+ * Navigation Component
+ *
+ * Renders the navigation menu with links to different pages.
+ *
+ * @returns {JSX.Element} The rendered Navigation component.
+ */
 export function Navigation() {
+  const pathname = usePathname();
+
+  const menuItems = [
+    { href: "/", label: "Home", icon: Home },
+    { href: "/saved-searches", label: "Saved Searches", icon: Save },
+    { href: "/favourite-courses", label: "Favourite Courses", icon: Heart },
+    { href: "/applications", label: "Applications", icon: FileText },
+  ];
+
   return (
     <div>
       <div className="container mx-auto transition-all p-4">
         <NavigationMenu>
           <NavigationMenuList>
-            <NavigationMenuItem>
-              <Link href="/" legacyBehavior passHref>
-                <NavigationMenuLink className="flex flex-row items-center gap-2">
-                  <Home className="w-4 h-4" />
-                  Home
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/saved-searches" legacyBehavior passHref>
-                <NavigationMenuLink className="flex flex-row items-center gap-2">
-                  <Save className="w-4 h-4" />
-                  Saved Searches
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
-            <NavigationMenuItem>
-              <Link href="/favourite-courses" legacyBehavior passHref>
-                <NavigationMenuLink className="flex flex-row items-center gap-2">
-                  <Heart className="w-4 h-4" />
-                  Favourite Courses
-                </NavigationMenuLink>
-              </Link>
-            </NavigationMenuItem>
+            {menuItems.map((item) => (
+              <NavigationMenuItem key={item.href}>
+                <Button
+                  asChild
+                  variant={pathname === item.href ? "default" : "ghost"}
+                >
+                  <Link href={item.href} legacyBehavior passHref>
+                    <NavigationMenuLink className="flex flex-row items-center gap-2">
+                      <item.icon className="w-4 h-4" />
+                      {item.label}
+                    </NavigationMenuLink>
+                  </Link>
+                </Button>
+              </NavigationMenuItem>
+            ))}
           </NavigationMenuList>
         </NavigationMenu>
       </div>
