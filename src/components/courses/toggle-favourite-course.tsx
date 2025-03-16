a5f7339e36c340727190460525d7389c40fe6a3f
Nadun Malinda <bknandun@gmail.com>
2025-03-16 12:22:38 +0100

    update: use debounce with optimistic update

--- src/components/courses/toggle-favourite-course.tsx
+++ src/components/courses/toggle-favourite-course.tsx
@@ -1,13 +1,13 @@
 "use client";
 
 import { cn } from "@/lib/utils";
-import { Button } from "../ui/button";
+import { Button } from "@/components/ui/button";
 import { Course } from "@/types/courses";
 import { Heart } from "lucide-react";
-import { useState, useOptimistic } from "react";
-import { useFormStatus } from "react-dom";
+import { useState, useOptimistic, useTransition } from "react";
 import { toggleFavouriteCourseAction } from "@/actions/course/toggle-favourite-course-action";
 import { toast } from "sonner";
+import { useDebouncedCallback } from "@/hooks/use-debounced-callback";
 
 /**
  * ToggleFavouriteCourse Component
@@ -20,60 +20,58 @@ import { toast } from "sonner";
  */
 export function ToggleFavouriteCourse({ course }: { course: Course }) {
   const [isFavorite, setIsFavorite] = useState(Boolean(course.IsFavorite));
+  const [isPending, startTransition] = useTransition();
   const [optimisticCourse, setOptimisticCourse] = useOptimistic<
     Course,
     boolean
   >(course, (state, isFavorite) => ({ ...state, IsFavorite: isFavorite }));
+  const debouncedCallback = useDebouncedCallback(() => formAction(), 500);
 
-  const formAction = async () => {
-    const newIsfavourite = !isFavorite;
-    setIsFavorite(newIsfavourite);
-    setOptimisticCourse(newIsfavourite);
+  const formAction = () => {
+    startTransition(async () => {
+      const newIsfavourite = !isFavorite;
+      setIsFavorite(newIsfavourite);
+      setOptimisticCourse(newIsfavourite);
 
-    const { message, success } = await toggleFavouriteCourseAction({
-      isFavorite: newIsfavourite,
-      courseId: course.CourseId,
+      const { message, success } = await toggleFavouriteCourseAction({
+        isFavorite: newIsfavourite,
+        courseId: course.CourseId,
+      });
+
+      if (success) {
+        toast.success(message);
+      } else {
+        toast.error(message);
+      }
     });
-
-    if (success) {
-      toast.success(message);
-    } else {
-      toast.error(message);
-    }
   };
 
   return (
-    <form action={formAction}>
-      <SubmitButton course={optimisticCourse} />
-    </form>
-  );
-}
-
-function SubmitButton({ course }: { course: Course }) {
-  const { pending } = useFormStatus();
-
-  return (
-    <Button
-      type="submit"
-      variant="ghost"
-      size="icon"
-      className={cn(
-        "h-8 w-8 rounded-full transition-all-200",
-        course.IsFavorite
-          ? "text-red-500 hover:text-red-600 hover:bg-red-50"
-          : "text-muted-foreground hover:text-red-500 hover:bg-red-50"
-      )}
-      aria-label={
-        course.IsFavorite ? "Remove from favorites" : "Add to favorites"
-      }
-      disabled={pending}
-    >
-      <Heart
+    <form action={debouncedCallback}>
+      <Button
+        type="submit"
+        variant="ghost"
+        size="icon"
         className={cn(
-          "h-4 w-4 transition-all-200",
-          course.IsFavorite ? "fill-current" : ""
+          "h-8 w-8 rounded-full transition-all-200",
+          optimisticCourse.IsFavorite
+            ? "text-red-500 hover:text-red-600 hover:bg-red-50"
+            : "text-muted-foreground hover:text-red-500 hover:bg-red-50"
         )}
-      />
-    </Button>
+        aria-label={
+          optimisticCourse.IsFavorite
+            ? "Remove from favorites"
+            : "Add to favorites"
+        }
+        disabled={isPending}
+      >
+        <Heart
+          className={cn(
+            "h-4 w-4 transition-all-200",
+            optimisticCourse.IsFavorite ? "fill-current" : ""
+          )}
+        />
+      </Button>
+    </form>
   );
 }
