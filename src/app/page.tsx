5b44eba0cd6ce34b95ee2126880a47857037ea84
Nadun Malinda <bknandun@gmail.com>
2025-03-16 08:53:13 +0100

    remove: search component as it now directly usign in the page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
-import { Search } from "@/components/search/search";
+import { SearchInput } from "@/components/search/search-input";
+import { SearchResult } from "@/components/search/search-result";
 import { type QueryParams } from "@/types/filters";
+import { Suspense } from "react";
 
 export default async function Home({
   searchParams,
@@ -10,7 +12,25 @@ export default async function Home({
 
   return (
     <div className="container mx-auto transition-all animate-fade-in">
-      <Search urlSearchParams={urlSearchParams} />
+      <div className="space-y-6 pb-8 page-transition">
+        <div className="space-y-1">
+          <h1 className="text-3xl font-bold">Search Courses</h1>
+          <p className="text-muted-foreground">
+            Find the perfect course for your educational journey
+          </p>
+        </div>
+
+        <div className="lg:col-span-2 space-y-6">
+          <SearchInput />
+
+          <Suspense
+            key={`${urlSearchParams.q}${urlSearchParams.page}`}
+            fallback={<>Loading ...</>}
+          >
+            <SearchResult urlSearchParams={urlSearchParams} />
+          </Suspense>
+        </div>
+      </div>
     </div>
   );
 }
