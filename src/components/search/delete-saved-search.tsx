c06816bc90566f0181da5953c018a8ceba3da042
Nadun Malinda <bknandun@gmail.com>
2025-03-16 10:09:55 +0100

    update: delete saved searches component to use form actions directly

--- src/components/search/delete-saved-search.tsx
+++ src/components/search/delete-saved-search.tsx
@@ -1,44 +1,57 @@
 "use client";
 
 import { LoaderCircle, Trash2 } from "lucide-react";
-import { Button } from "../ui/button";
-import { useActionState, useEffect } from "react";
+import { Button } from "@/components/ui/button";
 import { deleteSearchAction } from "@/actions/search/delete-search-action";
 import { toast } from "sonner";
+import { useFormStatus } from "react-dom";
 
+/**
+ * Component to delete a saved search.
+ *
+ * @param {Object} props - Component props.
+ * @param {string} props.searchId - The ID of the search to delete.
+ * @returns {JSX.Element} - DeleteSavedSearch component.
+ */
 export function DeleteSavedSearch({ searchId }: { searchId: string }) {
-  const action = deleteSearchAction.bind(null, searchId);
-  const [state, formAction, isPending] = useActionState(action, {
-    success: null,
-    message: "",
-  });
+  const formAction = async () => {
+    const { message, success } = await deleteSearchAction(searchId);
 
-  useEffect(() => {
-    // prevent showing the toast in the initial render.
-    if (state.success !== null) {
-      if (state.success) {
-        toast.success(state.message);
-      } else {
-        toast.error(state.message);
-      }
+    if (success) {
+      toast.success(message);
+    } else {
+      toast.error(message);
     }
-  }, [state.success, state.message]);
+  };
 
   return (
     <form action={formAction}>
-      <Button
-        variant="ghost"
-        size="icon"
-        className="h-8 w-8 text-muted-foreground hover:text-destructive hover:bg-destructive/10 -mt-1 -mr-2"
-        aria-label="Delete saved search"
-        disabled={isPending}
-      >
-        {isPending ? (
-          <LoaderCircle className="h-4 w-4 mr-2 animate-spin" />
-        ) : (
-          <Trash2 className="h-4 w-4" />
-        )}
-      </Button>
+      <SubmitButton />
     </form>
+  );
+}
+
+/**
+ * Submit button component with loading state.
+ *
+ * @returns {JSX.Element} - SubmitButton component.
+ */
+function SubmitButton() {
+  const { pending } = useFormStatus();
+
+  return (
+    <Button
+      variant="ghost"
+      size="icon"
+      className="h-8 w-8 text-muted-foreground hover:text-destructive hover:bg-destructive/10 -mt-1 -mr-2"
+      aria-label="Delete saved search"
+      disabled={pending}
+    >
+      {pending ? (
+        <LoaderCircle className="h-4 w-4 animate-spin" />
+      ) : (
+        <Trash2 className="h-4 w-4" />
+      )}
+    </Button>
   );
 }
