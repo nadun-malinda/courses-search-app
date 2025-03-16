9e21bf27f60aad33c30c14849c95019b003bc0f4
Nadun Malinda <bknandun@gmail.com>
2025-03-16 07:10:13 +0100

    update: use pagination in the search result page

--- src/components/search/search-result.tsx
+++ src/components/search/search-result.tsx
@@ -1,13 +1,21 @@
 import { CourseList } from "../courses/course-list";
 import { QueryParams } from "@/types/filters";
 import { dbFetchCourses } from "@/lib/data/course/db-fetch-courses";
+import { PaginationWrap } from "../pagination/pagination-wrap";
+import { PAGE_LIMIT } from "@/lib/contstants";
 
 export async function SearchResult({
   urlSearchParams,
 }: {
   urlSearchParams: QueryParams;
 }) {
-  const courses = await dbFetchCourses(urlSearchParams);
+  const page = Number(urlSearchParams.page) || 1;
+  const limit = PAGE_LIMIT;
+  const { courses, totalCount } = await dbFetchCourses(
+    urlSearchParams,
+    page,
+    limit
+  );
 
   console.log(">>> courses: ", courses);
 
@@ -16,10 +24,16 @@ export async function SearchResult({
       <h2 className="text-xl font-semibold flex items-center">
         Search Results
         <span className="ml-2 text-sm font-normal text-muted-foreground">
-          ({courses.length} courses found)
+          (Showing {courses.length} out of {totalCount} results)
         </span>
       </h2>
       <CourseList courses={courses} />
+      <PaginationWrap
+        page={page}
+        totalCount={totalCount || 0}
+        pageSize={limit}
+        pageSearchParam="page"
+      />
     </div>
   );
 }
